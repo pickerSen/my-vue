f1c978abef1e80a7e2b76597da128684ad3e0235
pickerSen <824819567@qq.com>
2017-08-01 01:46:03 +0800

    go ahead

--- main.js
+++ main.js
@@ -25,19 +25,30 @@ var app3 = new Vue({
 	})
 
 //处理用户输入
-var app3 = new Vue({
+var app4 = new Vue({
 		el: '#app-4',
 		data: {
+			inputMessage: '默认',
 			message: 'Hello vue2.0练习',
 			buttonName: '按钮（反转）',
 			button2Name: '切换',
+			i: 0
 		},
 		methods: {
 			reverseMessage: function () {
 				this.message = this.message.split(' ').reverse().join(' ')
 			},
 			clickEvent: function () {
-				
+
+				console.log('切换')
+				console.log(this.i)
+				let message = 'Hello vue2.0练习'
+				if (this.i%2 == 1){
+					this.message = message	
+				} else {
+					this.message = this.inputMessage
+				}				
+				this.i = this.i + 1
 			}
 		}
 	})
